71085308250eadeabd1cdc613ae3e8310ed33869
SilkooDarze <tsaotsaotsao51510@gmail.com>
2020-02-28 20:49:37 +0800

    add

--- bot.py
+++ bot.py
@@ -1,27 +1,38 @@
 import os
 import discord
-from dotenv import load_dotenv
 
-load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+token = 'NTkwMTc0NDEzMTMyMDA1Mzk2.XlfYqw.8TrdNWZxprCS5dYvaYb4LWiNaIY'
 
 client = discord.Client()
+
+data = {'virus-game': {'start-key': ['霹靂卡霹靂拉拉 波波力那貝貝魯多 武漢殺遊戲 掙扎吧 在血和暗的深淵裡 痛苦吧 在仇與恨的地獄中！', '\/\/μ#4/v |<!11 &4^^3, 57@Я7!']}}
 
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    print(client.guilds[1].channels)
+    # print(client.guilds[1].channels)
 
 
 @client.event
 async def on_message(message):
+
+    if message.content == '+1':
+        data['virus-game']['game-lobby'][str(message.guild.id) + ':' + str(message.channel.id)]['players'][message.author.id] = {'player-name': message.author.name, 'role': 0}
+        await message.add_reaction("👍")
+
     if message.author == client.user:
         return
 
-    if message.content == "霹靂卡霹靂拉拉 波波力那貝貝魯多 武漢殺遊戲 掙扎吧 在血和暗的深淵裡 痛苦吧 在仇與恨的地獄中！":
-        print(f'virus game start at guild {message.guild.name} ID:{message.guild.id} ')
-        return
+    for key in data['virus-game']['start-key']:  # check key
+        if message.content == key:
+            data['virus-game']['game-lobby'] = {str(message.guild.id) + ':' + str(message.channel.id): {'players': {}}}
+
+            await message.channel.send('''```javascript
+武漢殺開始報名
+請在下面打'+1'，由我先做一個示範，我會按讚表示確認。
+若確定所有人都已確認加入了，請輸入'Everyone has joined.'```''')
+            await message.channel.send('+1')
 
 
 client.run(token)
---
+++ speaker.py
@@ -0,0 +1,58 @@
+import os
+import discord
+import gtts
+
+token = 'NTkwMTc0NDEzMTMyMDA1Mzk2.XlfYqw.8TrdNWZxprCS5dYvaYb4LWiNaIY'
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    global voice_client
+    print(f'{client.user} has connected to Discord!')
+
+    print(client.guilds)
+
+    now_guild = client.guilds[0]
+    now_text_channel = now_guild.channels[0]
+    now_voice_channel = ''
+
+    while True:
+        input_str = input(f'[{now_guild}][{now_text_channel}]:')
+        if input_str.split(' ')[0] == '/connect_voice_channel':
+            for channel in now_guild.channels:
+                if channel.name == input_str.split(' ')[1]:
+                    await channel.connect()
+                    now_voice_channel = channel
+                    discord.VoiceClient = discord.utils.get(client.voice_clients, guild=now_guild)
+
+                    break
+
+        if input_str.split(' ')[0] == '/cd':
+            print(input_str.split(' '))
+            for guild in client.guilds:
+                if guild.name == input_str.split(' ')[1]:
+                    now_guild = guild
+                    break
+            for channel in now_guild.channels:
+                if channel.name == input_str.split(' ')[2]:
+                    now_text_channel = channel
+                    break
+
+        if input_str.split(' ')[0] == '/into_speak_mode':
+            while True:
+                gonna_speak = input(f'[{now_guild}][{now_text_channel}](Speaking):')
+                tts = gtts.gTTS(text=gonna_speak, lang='zh-tw')
+                tts.save("gonna_speak.mp3")
+                audio_source = discord.FFmpegPCMAudio('gonna_speak.mp3')
+                discord.VoiceClient.play(source=audio_source, after=None)
+                # player = now_voice_channel.create_ffmpeg_player('gonna_speak.mp3', after=lambda: print('done'))
+                # player.start()
+        # if input_str.split(' ')[0] == '/disconnect_voice_channel':
+        #     for channel in now_guild.channels:
+        #         if channel.name == input_str.split(' ')[1]:
+        #             await channel.disconnect()
+        #             break
+
+client.run(token)
